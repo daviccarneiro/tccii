3a9837f9003724159aa3ea08c0d0670ae7f5fd56
Davi Carneiro <daviccarneiro@icloud.com>
2024-11-08 13:24:04 -0300

    update openai version 3

--- app.py
+++ app.py
@@ -39,17 +39,14 @@ def enviar_para_notion(dados, especialidade):
 # Função para obter a especialidade recomendada usando a API da OpenAI com o novo formato
 def obter_especialidade_recomendada(queixa):
     try:
-        response = openai.ChatCompletion.create(
+        response = openai.Completion.create(
             model="gpt-4-turbo",  # Atualize para um modelo válido
-            messages=[
-                {"role": "system", "content": "Você é um assistente que recomenda especialidades odontológicas."},
-                {"role": "user", "content": f"A queixa do paciente é: '{queixa}'. Responda com apenas o nome da especialidade odontológica recomendada para este caso, sem explicações adicionais. Você poderá apenas escolher entre as opções: Ortodontia, Endodontia, Periodontia, Cirurgia, Dentística, Prótese, Odontopediatria, Estomatologia, Radiologia, Harmonização Facial e Implantodontia. Não coloque o ponto final na mensagem."}
-            ],
+            prompt=f"A queixa do paciente é: '{queixa}'. Responda com apenas o nome da especialidade odontológica recomendada para este caso, sem explicações adicionais. Você poderá apenas escolher entre as opções: Ortodontia, Endodontia, Periodontia, Cirurgia, Dentística, Prótese, Odontopediatria, Estomatologia, Radiologia, Harmonização Facial e Implantodontia.",
             max_tokens=10,
             temperature=0
         )
         
-        especialidade = response['choices'][0]['message']['content'].strip()
+        especialidade = response['choices'][0]['text'].strip()
         return especialidade
     except Exception as e:
         st.error(f"Erro ao consultar a API da OpenAI: {e}")
